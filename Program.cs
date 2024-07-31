9e67cd8d3402eba3487bf2937695191848621d53
Marious <88784666+Icescottea@users.noreply.github.com>
2024-07-31 20:32:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just to run the program.");
+            Console.ReadKey();
+        }
+    }
+}
